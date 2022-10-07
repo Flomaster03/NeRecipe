2c8d51782933efab7dd11e48b890d24e7b4c94d0
Denis Vetokhin <Tornado-000@rambler.ru>
2022-10-07 23:02:51 +0300

    base commit

---
+++ app/src/main/java/ru/netology/recipes/ui/ui/FilterFragment.kt
@@ -0,0 +1,6 @@
+package ru.netology.recipes.ui.ui
+
+import androidx.fragment.app.Fragment
+
+class FilterFragment : Fragment() {
+}
---
+++ app/src/main/java/ru/netology/recipes/ui/ui/MainActivity.kt
@@ -0,0 +1,32 @@
+package ru.netology.recipes.ui.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.isVisible
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import ru.netology.recipes.R
+import ru.netology.recipes.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        bottomNavigationView.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, _, arguments ->
+            binding.bottomNavigation.isVisible =
+                arguments?.getBoolean("ShowAppBar", false) == true
+        }
+    }
+
+}
