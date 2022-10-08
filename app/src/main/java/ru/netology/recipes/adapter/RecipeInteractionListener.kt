fd9d23db25c21de9a7d6cf23f549d6f95ee8c639
Denis Vetokhin <Tornado-000@rambler.ru>
2022-10-08 22:42:48 +0300

    before changes

---
+++ app/src/main/java/ru/netology/recipes/adapter/RecipeInteractionListener.kt
@@ -0,0 +1,14 @@
+package ru.netology.nerecipe.adapter
+
+import ru.netology.recipes.dto.Recipe
+
+interface RecipeInteractionListener {
+    fun onRemoveClicked(recipe: Recipe)
+    fun onEditClicked(recipe: Recipe)
+    fun onFavouriteClicked(recipeId: Long)
+    fun onSearchClicked(text: String)
+    fun onCreateClicked()
+    fun updateContent(id: Long, title: String, authorName: String, categoryRecipe: String, textRecipe: String)
+    fun onSaveClicked(title: String, authorName: String, categoryRecipe: String, textRecipe: String)
+    fun onSingleRecipeClicked(recipe: Recipe)
+}
---
+++ app/src/main/java/ru/netology/recipes/db/Recipes.kt
@@ -0,0 +1,22 @@
+package ru.netology.nerecipe.dto
+
+import ru.netology.recipes.dto.Recipe
+
+
+internal fun RecipeEntity.toModel() = Recipe(
+    id = id,
+    title = title,
+    authorName = authorName,
+    categoryRecipe = categoryRecipe,
+    textRecipe = textRecipe,
+    isFavourite = isFavourite,
+)
+
+internal fun Recipe.toEntity() = RecipeEntity(
+    id = id,
+    title = title,
+    authorName = authorName,
+    categoryRecipe = categoryRecipe,
+    textRecipe = textRecipe,
+    isFavourite = isFavourite,
+)
